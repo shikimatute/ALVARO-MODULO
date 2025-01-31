9715e2b88a5645dc9a3fcd4f93b1751fd09a3f6a
shikimatute <matutedeandrade@gmail.com>
2025-01-31 09:30:07 -0300

    Añade validación de errores y detención automática tras 2 fallos

--- analysis-module/dist/server.js
+++ analysis-module/dist/server.js
@@ -1,22 +1,22 @@
 import express from "express";
 //import axios from "axios";
-import { triggerAnalysis_Module } from "./analysis-module.js";
+import { triggerAnalysis_Module } from "./analysis-module";
 const app = express();
 //const mockAxios = axios.create();
 // Middleware para parsear JSON
 app.use(express.json());
 app.get("/", (_, res) => {
-  res.send("Servidor funcionando correctamente");
+    res.send("Servidor funcionando correctamente");
 });
 // Ruta de prueba
 app.post("/api/submit", async (req, res) => {
-  try {
-    const data = req.body; // Asegúrate de enviar datos en formato JSON
-    console.log("Datos recibidos:", data);
-    res.status(200).json({ message: "Datos recibidos correctamente", data });
-    const formulario = data["event_datasets"]["zonas"];
-    triggerAnalysis_Module(formulario);
-    /*mockAxios.interceptors.request.use((config) => {
+    try {
+        const data = req.body; // Asegúrate de enviar datos en formato JSON
+        console.log("Datos recibidos:", data);
+        res.status(200).json({ message: "Datos recibidos correctamente", data });
+        const formulario = data["event_datasets"]["zonas"];
+        triggerAnalysis_Module(formulario);
+        /*mockAxios.interceptors.request.use((config) => {
           return Promise.resolve({
             data: {
               message: "Datos simulados correctamente procesados",
@@ -38,13 +38,14 @@ app.post("/api/submit", async (req, res) => {
             console.error("Error al llamar al endpoint de análisis:", error);
           }
         } */ //para alvaro.
-  } catch (error) {
-    console.error("Error al llamar a la API:", error);
-    res.status(500).json({ message: "Error al llamar a la API externa." });
-  }
+    }
+    catch (error) {
+        console.error("Error al llamar a la API:", error);
+        res.status(500).json({ message: "Error al llamar a la API externa." });
+    }
 });
 // Inicia el servidor
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
